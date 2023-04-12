f01a6d82dd9ffe8183ce60d73d5953aa8fda2979
Harrisonguy <hguy@flemingcollege.ca>
2023-04-12 00:56:07 -0400

    Update apod_desktop.py

--- apod_desktop.py
+++ apod_desktop.py
@@ -20,7 +20,6 @@ import sqlite3
 import apod_api
 import hashlib
 import re
-import calendar
 # Global variables
 image_cache_dir = None  # Full path of image cache directory
 image_cache_db = None   # Full path of image cache database
